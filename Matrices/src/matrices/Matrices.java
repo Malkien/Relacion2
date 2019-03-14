80a39f55bdf8f93379170570f434aaea041f318d
Malkien <kevin1cuenca@gmail.com>
2019-03-14 19:46:42 +0100

    Apartado ultimo de matrices ok

--- Matrices/src/matrices/Matrices.java
+++ Matrices/src/matrices/Matrices.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author 1DAM
+ * @author Malkien
  */
 public class Matrices {
 
@@ -36,6 +36,10 @@ public class Matrices {
         //|    “techo”         “mueño”	    “sungo”          |
         String[][] matriz2={{"yo quird!","descifero","passworar"},{"techo","mueño","sungo"}};
         System.out.println(Matrices.mostrarMatriz(Matrices.descifrarMatriz(matriz2)));
+        //|  “voy a”  “aprobar”  “arrasando”  |
+        //|  “quiero” “tener un” “diez”     |
+        String[][] matriz3={{"voy a","aprobar","arrasando"},{"quiero","tener un","diez"}};
+        System.out.println(Matrices.mostrarMatriz(Matrices.descifrarInversaMatriz(matriz3)));
     }
     /**
      * Cambia los tres últimos digitos de cada String de la matriz Siendo de derecha a izquierda las filas pares y de 
@@ -61,6 +65,36 @@ public class Matrices {
                         m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][j-1].substring(m1[i][j-1].length()-3);
                     }else{
                         m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][m2[0].length-1].substring(m1[i][m2[0].length-1].length()-3);
+                    }
+                }
+            }
+        }
+        return m2;
+    }
+    /**
+     * Cambia los tres últimos digitos de cada String de la matriz Siendo de derecha a izquierda las filas pares y de 
+     * izquierda a derecha las impares.
+     * @param m1 la matriz que usamos de referencia para crear la nueva matriz
+     * @return  La nueva matriz modificada
+     */
+    public static String[][] descifrarInversaMatriz(String[][] m1){
+        String[][] m2=new String[m1.length][m1[0].length];
+        
+        for (int i = 0; i < m2.length; i++) {
+            if(i%2==0){
+                for (int j = m2[0].length-1; j >= 0 ; j--) {
+                    if(j>0){
+                        m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][j-1].substring(m1[i][j-1].length()-3);
+                    }else{
+                        m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][m2[0].length-1].substring(m1[i][m2[0].length-1].length()-3);
+                    }
+                }
+            }else{
+                for (int j = 0; j < m2[0].length; j++) {
+                    if(j<m2[0].length-1){
+                        m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][j+1].substring(m1[i][j+1].length()-3);
+                    }else{
+                        m2[i][j]=m1[i][j].substring(0,m1[i][j].length()-3)+m1[i][0].substring(m1[i][0].length()-3);
                     }
                 }
             }
